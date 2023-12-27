41b83b4b699ec04620c85c86bf9e135ce25ebd1e
Nazım Çimen <cimennazim.27@gmail.com>
2023-12-27 04:21:43 +0300

    TEST

--- lib/init/init.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get_storage/get_storage.dart';
-import 'package:pharmacy_app/utility/constants.dart';
-
-Future<void> initializeApp() async {
-  await GetStorage.init();
-  popUpCity.text =  box.read(boxKeyValueOfCity) ?? chooseCityTxt;
-  popUpDistrict.text =  box.read(boxKeyValueOfDistrict) ?? chooseDistrictTxt;
-  
-}
